f4d6fe28529268c6a5a0a2a83d16ad017e7bf3af
anisssal <anis.alathiif@gmail.com>
2018-04-02 11:36:28 +0700

    Update

--- app/src/main/java/com/iman/inc/doadzikirhisnulmuslim/MyConstant.java
+++ app/src/main/java/com/iman/inc/doadzikirhisnulmuslim/MyConstant.java
@@ -1,9 +1,19 @@
 package com.iman.inc.doadzikirhisnulmuslim;
 
-/**
- * Created by z on 20/03/18.
- */
+import android.provider.BaseColumns;
+
 
 public class MyConstant  {
+    public static final class TabelJudul implements BaseColumns {
+        public static final String TABLE_NAME  = "judulDoa";
+        public static final String COL_idUtama = "_idUtama";
+        public static final String COL_IDBag = "bagID";
+        public static final String COL_BagName = "BagName";
+        public static final String COL_DoaName  = "DuaName";
+        public static final String COL_DuaId = "duaID";
+        public static final String COL_Kat  = "Kat";
+
+
+    }
 
 }
